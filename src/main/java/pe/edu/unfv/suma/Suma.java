16d49d98c234d453f08b65c33e50e45d58067cb6
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-09-11 16:53:23 -0500

    avanzando

---
+++ src/main/java/pe/edu/unfv/suma/Suma.java
@@ -0,0 +1,11 @@
+package pe.edu.unfv.suma;
+
+public class Suma {
+
+	int suma(int x, int y) {
+		
+		int resultado = x + y;
+		
+		return resultado;
+	}
+}
---
+++ src/test/java/pe/edu/unfv/sumaTest/SumaTest.java
@@ -0,0 +1,5 @@
+package pe.edu.unfv.sumaTest;
+
+public class SumaTest {
+
+}
